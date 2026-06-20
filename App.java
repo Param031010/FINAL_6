92210b0dba3391a8c2053e04ace97ddf078e1fc2
Param <paramraval2005@gmail.com>
2026-06-20 17:43:56 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.program44.maven.pr4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Mike!" );
+    }
+}
